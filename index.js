aed9a4944b598bde4d542d82eca6add8c4ddc9ac
kamrul25 <kamrul.coder01@gmail.com>
2023-07-23 14:22:40 +0600

    connect mongoDB sucessfully

--- index.js
+++ index.js
@@ -9,6 +9,44 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+//connect mongoDB
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sunshinecluster.2tjkdbc.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const allJobInfo = client.db(`${process.env.DB_USER}`).collection("allJobInfo")
+
+    app.get("/jobs", async(req, res)=>{
+        const result = await allJobInfo.find({}).toArray()
+        res.json(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res)=>{
     res.json("Server is running")
 })
